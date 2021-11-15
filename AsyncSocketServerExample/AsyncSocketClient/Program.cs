ffcac8861820bbfbe84802fd0f21a8a872134722
Andre Wells <Andre.Wells@korbitecinc.com>
2021-11-15 19:23:53 +0200

    Added Async Socket and Client example

---
+++ AsyncSocketServerExample/AsyncSocketClient/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+
+Console.WriteLine("Hello, World!");
+using var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+await socket.ConnectAsync(new IPEndPoint(IPAddress.Loopback, 8080));
+
+Console.WriteLine("Type into the console to echo the contents:");
+
+var ns = new NetworkStream(socket);
+Console.WriteLine("[Reading/Sending Data ...]");
+var readTask = Console.OpenStandardInput().CopyToAsync(ns);
+Console.WriteLine("[Recieving Data ...]");
+var writeTask = ns.CopyToAsync(Console.OpenStandardOutput());
+
+// Quit if any of the tasks complete
+await Task.WhenAny(readTask, writeTask);
---
+++ AsyncSocketServerExample/AsyncSocketServer/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+
+Console.WriteLine("Hello, World!");
+
+using var listenSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+listenSocket.Bind(new IPEndPoint(IPAddress.Loopback, 8080));
+
+Console.WriteLine($"Listening on {listenSocket.LocalEndPoint}");
+
+listenSocket.Listen();
+
+while (true)
+{
+    // Wait for a new connection to arrive
+    var connection = await listenSocket.AcceptAsync();
+    Console.WriteLine("Accepting Connection... ");
+
+    // We got a new connection spawn a task to so that we can echo the contents of the connection
+    _ = Task.Run(async () =>
+    {
+        var buffer = new byte[4096];
+        try
+        {
+            while (true)
+            {
+                Console.WriteLine("Recieving ... ");
+                int read = await connection.ReceiveAsync(buffer, SocketFlags.None);
+                if (read == 0)
+                {
+                    break;
+                }
+                Console.WriteLine("Sending ... ");
+                await connection.SendAsync(buffer[..read], SocketFlags.None);
+            }
+        }
+        finally
+        {
+            Console.WriteLine("Connection Closed.");
+            connection.Dispose();
+        }
+    });
+}
