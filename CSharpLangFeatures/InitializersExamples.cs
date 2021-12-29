808d33f6f80073fa119e573accd376356e30d97c
Andre Wells <Andre.Wells@korbitecinc.com>
2021-12-29 20:15:06 +0200

    WIP lang features

---
+++ CSharpLangFeatures/InitializersExamples.cs
@@ -0,0 +1,26 @@
+class ReadOnlyProperties
+{
+
+    public string? FirstName { get; } = null;
+    public string LastName { get; } = string.Empty;
+
+    public ReadOnlyProperties(string fname, string lname)
+    {
+        FirstName = fname;
+        LastName = lname;
+    }
+
+    static ReadOnlyProperties Create() => new ReadOnlyProperties("a", "b");
+}
+
+class InitAccessor
+{
+    public string FirstName { get; init; } = string.Empty;
+    public string LastName { get; init; } = string.Empty;
+
+    static InitAccessor Create() => new InitAccessor
+    {
+        FirstName = "a",
+        LastName = "b"
+    };    
+}
