7af268c6b00ebdb46242f19b5b7d50f6d9a4ceea
Andre Wells <awells@bscglobal.com>
2022-05-23 09:19:57 +0200

    Added cancellation example

---
+++ WebApiTimeoutCancellation/WebApiTimeoutCancellation.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Fetching data ...");
+HttpClient client = new HttpClient();
+client.Timeout = TimeSpan.FromSeconds(6);
+var result = await client.GetStringAsync("https://localhost:7281/WeatherForecast");
+Console.WriteLine(result);
+
+
---
+++ WebApiTimeoutCancellation/WebApiTimeoutCancellation/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTimeoutCancellation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public async Task<IEnumerable<WeatherForecast>> Get(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"Request {System.Diagnostics.Activity.Current?.Id}: Fetching weather information");
+            await Task.Delay(5000, cancellationToken);
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var results = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            _logger.LogInformation($"Request {System.Diagnostics.Activity.Current?.Id}: Number of results: {results.Count()}");
+
+            return results;
+        }
+    }
+}
